f37dc212150a796bed77c0596a7c76ad4dac0c43
ERIK YAHIR DIAZ MARTINEZ <I20050014@monclova.tecnm.mx>
2020-09-07 16:00:32 -0500

    hola

---
+++ primera/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mi programacion!");
+        }
+    }
+}
